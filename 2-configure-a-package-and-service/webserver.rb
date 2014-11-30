a9311203a9ccae2c80263239ae5578d6b9335436
rojoangel <vagrant@localhost.localdomain>
2014-11-30 12:34:00 +0100

    adding 2nd tutorial chapter

---
+++ 2-configure-a-package-and-service/webserver.rb
@@ -0,0 +1,17 @@
+package 'httpd'
+
+service 'httpd' do
+  action [:start, :enable]
+end
+
+file '/var/www/html/index.html' do
+  content '<html>
+  <body>
+    <h1>hello world</h1>
+  </body>
+</html>'
+end
+
+service 'iptables' do
+  action :stop
+end
